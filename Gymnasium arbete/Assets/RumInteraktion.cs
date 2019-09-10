aecdb9307108786256538fe389f47dbe859f2c0f
ViktorBakken <55138236+ViktorBakken@users.noreply.github.com>
2019-09-10 13:27:23 +0200

    Flyttade över gymnasium arbetet till github

---
+++ Gymnasium arbete/Assets/RumInteraktion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumInteraktion : MonoBehaviour
+{
+
+    private Renderer lampa;
+
+    private void Start()
+    {
+        lampa = GameObject.FindGameObjectWithTag("Lampa").GetComponent<Renderer>();
+    }
+
+    public void LampBlinka()
+    {
+        
+    }
+}
