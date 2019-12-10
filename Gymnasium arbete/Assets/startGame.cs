8bd8fff86109f5e5f49cd0e3b74562346055dc65
ViktorBakken <55138236+ViktorBakken@users.noreply.github.com>
2019-12-10 15:32:58 +0100

    hej hå

---
+++ Gymnasium arbete/Assets/startGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    [SerializeField]
+    private Scene game;
+    
+    
+    // Start is called before the first frame update2
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
